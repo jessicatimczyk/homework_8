149c741020f42f7cde0a0e6502afb1b193f70c54
jessicatimczyk <jtimczyk@andrew.cmu.edu>
2020-11-30 13:33:34 -0500

    anime library working!

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-import anime from 'anime.es.js';
+function onload() {
 
-anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-});
-
-//anime({
-//    targets: 'div',
-//    left: '240px',
-//    backgroundColor: '#FFF',
-//    borderRadius: ['0%', '50%'],
-//    easing: 'easeInOutQuad'
-//});
+    anime({
+        targets: 'div',
+        translateX: 250,
+        rotate: '1turn',
+        backgroundColor: '#FFF',
+        duration: 800
+    });
+}

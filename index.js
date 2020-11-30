2fb83063501d3fe33a4bf5f40edddd655105eaee
jessicatimczyk <jtimczyk@andrew.cmu.edu>
2020-11-30 08:04:45 -0500

    getting started

---
+++ index.js
@@ -0,0 +1,11 @@
+//import anime from 'animejs/lib/anime.es.js';
+import anime from 'anime-master/lib/anime.es.js';
+const anime = require('animejs');
+
+anime({
+    targets: 'div',
+    translateX: 250,
+    rotate: '1turn',
+    backgroundColor: '#FFF',
+    duration: 800
+});

8af0b464fde8ecedf8aba004269df801c3a73f6d
jessicatimczyk <jtimczyk@andrew.cmu.edu>
2020-12-02 09:57:57 -0500

    cleaned up different libraries im using and worked on homepage
    added bootstrap and got rid of p5 for homepage for now

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+
+
+
 function onload() {
 
-    anime({
-        targets: 'div',
-        translateX: 250,
-        rotate: '1turn',
-        backgroundColor: '#FFF',
-        duration: 800
-    });
+//    anime({
+               //        targets: 'div',
+               //        translateX: 250,
+               //        rotate: '1turn',
+               //        backgroundColor: '#FFF',
+               //        duration: 800
+               //    });
+
 }
+
+
+
+
+//let sketch = function (p) {
+//    p.setup = function () {
+//        p.createCanvas(100, 100);
+//        p.background(0);
+//
+//        yellow_c = color(245, 225, 56);
+//        brown_c = color(161, 89, 17);
+//
+//        fill(yellow_c);
+//        stroke(brown_c);
+//        strokeWeight(5);
+//        circle(200, 200, 100);
+//    }
+//};
+//let node = document.createElement('div');
+//new p5(sketch, node);
+//node.setAttribute("id", "gobutton");
+//window.document.getElementsByTagName('body').appendChild(node);
+////document.getElementById("helo").appendChild(node);
+//
+//function draw() {
+//    let homepage = document.getElementById("helo");
+//    background(255, 0);
+//    var gobutn = rect(100, 100, 50, 70);
+//    //    var gobutn = document.createElement("button");
+//    //    var butntxt = document.createTextNode("Start Learning");
+//    //    gobutn.setAttribute("id", "gobutton");
+//    //    gobutn.appendChild(butntxt);
+//
+//    homepage.appendChild(gobutn);
+//    polygon(windowWidth / 2, windowHeight / 2, 50, 6);
+//}
+//
+//function polygon(x, y, radius, points) {
+//    let angle = TWO_PI / points;
+//    beginShape();
+//    for (let i = 0; i < TWO_PI; i += angle) {
+//        let sx = x + cos(i) * radius;
+//        let sy = y + sin(i) * radius;
+//        vertex(sx, sy);
+//    }
+//    endShape(CLOSE);
+//}
